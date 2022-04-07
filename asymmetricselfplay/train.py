d02447f6db79c196678104d6316e869c03b344aa
Rodrigo Perez-Vicente <r.l.p.v96@gmail.com>
2022-04-06 22:28:54 -0400

    update weigts priors

--- asymmetricselfplay/train.py
+++ asymmetricselfplay/train.py
@@ -10,18 +10,58 @@ from gym import spaces
 import numpy as np
 from collections import OrderedDict
 from ray.tune.integration.wandb import WandbLoggerCallback
+from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray import tune
 import ray
 import argparse
+import itertools
+
+prior_alice_policies_names = ["prior_alice_policy_" + str(i+1) for i in range(4)]
+prior_bob_policies_names = ["prior_bob_policy_" + str(i+1) for i in range(4)]
+
+class AsymSelfPlayCallback(DefaultCallbacks):
+    def __init__(self):
+        super().__init__()
+
+        self._alice_prior_pol_iter = itertools.cycle(prior_alice_policies_names)
+        self._bob_prior_pol_iter = itertools.cycle(prior_bob_policies_names)
+    
+    def on_train_result(self, *, trainer, result, **kwargs):
+
+        # Only one agent is allowed to compete against prior versions of its oponent per training iteration
+        # Modify policy mapping function for each iteration
+        if result["training_iteration"] % 2 == 0:
+            policy_mapping_function = prior_alice_policy_mapping_fn
+        else:
+            policy_mapping_function = prior_bob_policy_mapping_fn
+        
+        def _set(worker):
+            worker.set_policy_mapping_fn(policy_mapping_function)
+        
+        trainer.workers.foreach_worker(_set)
+
+        # Update one prior alice + bob policy with current policy weights
+        if result["training_iteration"] % 10 == 0:
+            alice_prior_pol = self._alice_prior_pol_iter.__next__()
+            bob_prior_pol = self._bob_prior_pol_iter.__next__()
+
+            current_alice_state = trainer.get_policy("alice_policy").get_state()
+            current_bob_state = trainer.get_policy("bob_policy").get_state()
+            pol_map = trainer.workers.local_worker().policy_map
+            pol_map[alice_prior_pol].set_state(current_alice_state)
+            pol_map[bob_prior_pol].set_state(current_bob_state)
+
+            trainer.workers.sync_weights(policies=[alice_prior_pol, bob_prior_pol])
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-objects", type=int, default=2)
-parser.add_argument("--num-workers", type=int, default=4)
+parser.add_argument("--num-workers", type=int, default=1)
 
 # == Observation dict keys ==
 # robot_state_keys = ["robot_joint_pos", "gripper_pos"]
 # obj_state_keys = ["obj_pos", "obj_rot", "obj_vel_pos", "obj_vel_rot", "obj_rel_pos", "obj_gripper_contact"]
 # goal_state_keys = ["goal_obj_pos", "goal_obj_rot", "rel_goal_obj_pos", "rel_goal_obj_rot"]
+
 
 def get_rllib_configs():
     args = parser.parse_args()
@@ -56,6 +96,7 @@ def get_rllib_configs():
     action_space = spaces.MultiDiscrete(np.array([11]*6))
 
     def policy_mapping_fn(agent_id, episode, worker, **kwargs):
+        # Choose 
         if agent_id == "alice":
             return "alice_policy"
         else:
@@ -70,14 +111,14 @@ def get_rllib_configs():
                     "number_of_objects": args.num_objects,
                     "dict_obs_space": observation_spaces[agent],
                     # == MLP ==
-                    "mlp_hiddens": [256] * 5,
+                    "mlp_hiddens": [512] * 5,
                     "mlp_activation": "relu",
                 },
                 # == LSTM ==
                 # Max seq len for training the LSTM, defaults to 20.
                 "max_seq_len": 20,
                 # Size of the LSTM cell.
-                "lstm_cell_size": 256,
+                "lstm_cell_size": 1024,
                 # Whether to feed a_{t-1} to LSTM (one-hot encoded if discrete).
                 "lstm_use_prev_action": True,
                 # Whether to feed r_{t-1} to LSTM.
@@ -85,7 +126,6 @@ def get_rllib_configs():
                 # Whether the LSTM is time-major (TxBx..) or batch-major (BxTx..).
                 "_time_major": False,
             },
-
             "gamma": 0.998,
             "lr": 3e-4,
             "lambda": 0.95,
@@ -95,32 +135,44 @@ def get_rllib_configs():
         } for agent in agents
     }
 
-
-    config = {
-        "env": "asym_self_play",
-        "num_workers": args.num_workers,
-        "num_envs_per_worker": 5,
-        "num_gpus": 0,
-        "rollout_fragment_length": 4096,
-        "batch_mode": "complete_episodes",
-        "framework": "torch",
-        "train_batch_size": 40960,
-        "sgd_minibatch_size": 128,
-        # sample reuse or epochs per training iterations
-        "num_sgd_iter": 3,
-        "multiagent": {
-            "policies": {
-                "alice_policy": policy.PolicySpec(policy_class=PPOTorchPolicy,
+    policies_dict = {"alice_policy": policy.PolicySpec(policy_class=PPOTorchPolicy,
                                                     observation_space=observation_spaces["alice"],
                                                     action_space=action_space,
                                                     config=policy_configs["alice"]),
-                "bob_policy": policy.PolicySpec(policy_class=BobTorchPolicy, 
+                     "bob_policy": policy.PolicySpec(policy_class=BobTorchPolicy, 
+                                                observation_space=observation_spaces["bob"], 
+                                                action_space=action_space, 
+                                                config=policy_configs["bob"])}
+    alice_prior_policies = {pol: policy.PolicySpec(policy_class=PPOTorchPolicy,
+                                                    observation_space=observation_spaces["alice"],
+                                                    action_space=action_space,
+                                                    config=policy_configs["alice"]) for pol in prior_alice_policies_names}
+    
+    bob_prior_policies = {pol: policy.PolicySpec(policy_class=BobTorchPolicy, 
                                                     observation_space=observation_spaces["bob"], 
                                                     action_space=action_space, 
-                                                    config=policy_configs["bob"]
-                                                ),
-            },
+                                                    config=policy_configs["bob"]) for pol in prior_bob_policies_names}
+   
+    policies_dict.update(alice_prior_policies)
+    policies_dict.update(bob_prior_policies)
+
+    config = {
+        "env": "asym_self_play",
+        "callbacks": AsymSelfPlayCallback,
+        "num_workers": args.num_workers,
+        "num_envs_per_worker": 1,
+        "num_gpus": 0,
+        "rollout_fragment_length": 400,
+        "batch_mode": "complete_episodes",
+        "framework": "torch",
+        "train_batch_size": 400,
+        "sgd_minibatch_size": 60,
+        # sample reuse or epochs per training iterations
+        "num_sgd_iter": 3,
+        "multiagent": {
+            "policies": policies_dict,
             "policy_mapping_fn": policy_mapping_fn,
+            "policies_to_train": ["alice_policy", "bob_policy"],
         },
         "sample_collector": MultiEpisodeCollector,
     }   
@@ -133,13 +185,30 @@ def get_rllib_configs():
 
     return config, stop
 
+def prior_alice_policy_mapping_fn(agent_id, episode, worker, **kwargs):
+    if agent_id == "bob":
+        return "bob_policy"
+    else:
+        return np.random.choice(["alice_policy", "prior_alice_policy_1", 
+                                    "prior_alice_policy_2", "prior_alice_policy_3", "prior_alice_policy_4"],1,
+                                    p=[.8, .1/2, .1/2, .1/2, .1/2])[0] 
+
+def prior_bob_policy_mapping_fn(agent_id, episode, worker, **kwargs):
+    if agent_id == "alice":
+        return "alice_policy"
+    else:
+        return np.random.choice(["bob_policy", "prior_bob_policy_1", 
+                                    "prior_bob_policy_2", "prior_bob_policy_3", "prior_bob_policy_4"],1,
+                                    p=[.8, .1/2, .1/2, .1/2, .1/2])[0]
+       
 def run_tune():
     config, stop = get_rllib_configs()
     tune_analysis = tune.run("PPO", config=config, stop=stop, checkpoint_freq=500, checkpoint_at_end=True, callbacks=[WandbLoggerCallback(
             project="AsymmetricSelfPlay",
             api_key_file="wandb_api_key",
-            log_config=True)])
-    
+            log_config=True,
+            mode="disabled")])
+
     return tune_analysis
 
 if __name__ == "__main__":
